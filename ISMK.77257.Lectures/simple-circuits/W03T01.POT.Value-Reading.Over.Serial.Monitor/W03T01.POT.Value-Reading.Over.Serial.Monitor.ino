469b6d6614cace9c9a46c88925669f83ec4e47bc
coshkun <cskncnr@gmail.com>
2017-03-16 08:51:37 +0300

    update

--- ISMK.77257.Lectures/simple-circuits/W03T01.POT.Value-Reading.Over.Serial.Monitor/W03T01.POT.Value-Reading.Over.Serial.Monitor.ino
+++ ISMK.77257.Lectures/simple-circuits/W03T01.POT.Value-Reading.Over.Serial.Monitor/W03T01.POT.Value-Reading.Over.Serial.Monitor.ino
@@ -3,7 +3,7 @@
  * @author: coskun
  * 
  * W03T01.Simple-Circutis.POT.Value-Reading.Over.Serial.Monitor
- * https://circuits.io/circuits/4270259-w02t05-rgb-control-over-serial-monitor
+ * 
  * 
  * Bu projede serial monitör.den kulanıcının ayarladığı pot
  * üzerinden voltaj seviyesi okunarak SM.den cıktı alınır.
@@ -21,6 +21,6 @@ Serial.begin(9600);
 void loop() {
   // put your main code here, to run repeatedly:
 val = analogRead(A0);
-Serial.println("Potun değeri: ");
-Serial.print(String(val* 10/1024));
+Serial.print("Potun degeri: ");
+Serial.println(String(val* 10/1024) + " Kohm");
 }
---
+++ ISMK.77257.Lectures/simple-circuits/W03T02.3POT.controlled.RGB.MulticolorLED/W03T02.3POT.controlled.RGB.MulticolorLED.ino
@@ -0,0 +1,34 @@
+/*
+ * ISM.77257.Lectures
+ * @author: coskun
+ * 
+ * W03T02.Simple-Circutis.3POT.controlled.RGB.MulticolorLED
+ * 
+ * 
+ * Bu projede 3 ayrı pottan okunan analog değerler,
+ * RGB çokrenkled üzerinde 3 ayrı renk kanalını 
+ * kontrol etmek içi kullanıldı
+ * 
+ * Not: Bu test.te simulator arızası yüzünden fiziksel devre 
+ * kullandık. 
+ */
+
+int O=12,R=11,B=10,G=9;
+int vR,vG,vB;
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int i=12; i>8 ; i--){ digitalWrite(i,OUTPUT);}
+}
+
+void loop() {
+  // read data by 10-bit
+  vR = analogRead(A0);
+  vG = analogRead(A1);
+  vB = analogRead(A2);
+
+  digitalWrite(O, HIGH); // fire the anode pin.
+  analogWrite(R, 255 - (vR/4));
+  analogWrite(G, 255 - (vG/4));
+  analogWrite(B, 255 - (vB/4));
+}
